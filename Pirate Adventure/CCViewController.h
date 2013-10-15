5b586391abacb5f4673b484ceeb28bfa6300caa8
Development <development@Marks-MacBook-Pro.local>
2013-10-14 22:39:29 -0700

    Initial Commit

---
+++ Pirate Adventure/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Pirate Adventure
+//
+//  Created by Development on 10/14/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
